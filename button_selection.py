df99fe527a8e8a6403cacb50ef5e6718e6be7428
yishai <yishairasowsky@gmail.com>
2023-03-22 19:09:11 +0200

    advice from gpt4 remove main()

--- button_selection.py
+++ button_selection.py
@@ -1,26 +1,38 @@
 import streamlit as st
+import re
 
-def main():
-    st.title("Button Selection App")
+def generate_response(input_text):
+    # Generate some response based on input text
+    response = f"You said: {input_text}. What would you like me to do next?"
+    return response
 
-    # Define the initial user input
-    user_input = st.text_input("Enter your input:")
+def get_next_responses(response):
+    # Split the response into segments based on numeric values
+    numbers = re.findall(r'\d+', response)
+    segments = re.split(r'\d+\.', response)
+    reply, suggestions = segments[0], segments[1:]
+    responses = {}
+    for i in range(len(numbers)):
+        responses[numbers[i]] = suggestions[i].strip()
+    return responses
 
-    while user_input:
-        # Display the current user input
-        st.write(f"Current input: {user_input}")
+# Define initial user input
+user_input = st.text_input("Enter some text here:")
 
-        # Display a set of three buttons for the current input
-        for i in range(3):
-            button_choice = st.button(f"{user_input} - Option {i+1}")
-            if button_choice:
-                user_input = f"{user_input} - Option {i+1}"
+# Loop to continue conversation until user cancels or terminates
+while user_input:
+    # Generate response based on user input
+    response = generate_response(user_input)
+    # Get next set of responses from the user via buttons
+    next_responses = get_next_responses(response)
+    # Create buttons for each response option
+    button_container = st.container()
+    cols = {}
+    with button_container:
+        cols['1'], cols['2'], cols['3'] = st.columns(3)
+        for key, val in next_responses.items():
+            if cols[key].button(next_responses[key]):
+                user_input = val
                 break
-
-        # Ask for a new user input
-        user_input = st.text_input("Enter your input:", value=user_input[len(user_input.split()[-1]):].strip())
-
-    st.write("Thanks for using the Button Selection App!")
-
-if __name__ == "__main__":
-    main()
+        else:
+            user_input = None
