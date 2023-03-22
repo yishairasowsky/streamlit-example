790a998aa4e611e57a8211fa3cd5ddba55ebc920
yishai <yishairasowsky@gmail.com>
2023-03-22 19:01:13 +0200

    keeps the old input bar and adds new buttons underneath

--- button_selection.py
+++ button_selection.py
@@ -4,27 +4,33 @@ def main():
     st.title("Button Selection App")
     
     # Define the initial set of user inputs and button options
-    user_input = st.text_input("Enter your input:")
+    user_inputs = st.text_input("Enter your input:", key="user_inputs").split("\n")
     button_options = ["Option 1", "Option 2", "Option 3"]
     
-    while user_input:
+    for i, user_input in enumerate(user_inputs):
+        if user_input.strip() == "":
+            continue
+        
         # Display the current user input
         st.write(f"Current input: {user_input}")
         
         # Display the button options for the current user input
-        for i in range(3):
-            button_choice = st.button(f"{button_options[i]}")
+        for j in range(3):
+            button_choice = st.button(f"{button_options[j]}", key=f"{i}-{j}")
             if button_choice:
-                user_input_value = user_input + f" - {button_options[i]}"
+                user_inputs[i] = f"{user_input} - {button_options[j]}"
                 break
         
-        # Ask for a new user input and reset the button options
-        new_user_input = st.text_input("Enter your input:", value=user_input_value[len(user_input.split()[-1]):].strip())
-        button_options = ["Option 1", "Option 2", "Option 3"]
+        # Add a new input bar if this is not the last iteration
+        if i != len(user_inputs) - 1:
+            user_inputs[i+1] = st.text_input("Enter your input:", value=user_inputs[i+1], key=f"user_input_{i+1}").strip()
         
-        # Update the user input variable
-        user_input = new_user_input if new_user_input != user_input_value else user_input
+        # Reset the button options
+        button_options = ["Option 1", "Option 2", "Option 3"]
     
+    # Join the user inputs and write the final message
+    final_input = "\n".join(user_inputs).strip()
+    st.write(f"Final input: {final_input}")
     st.write("Thanks for using the Button Selection App!")
 
 if __name__ == "__main__":
