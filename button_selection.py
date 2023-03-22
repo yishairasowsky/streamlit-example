cc0693c76544d6b0f206aa1ef6b98cd9d5c764f2
yishai <yishairasowsky@gmail.com>
2023-03-22 19:04:41 +0200

    always display a single set of three buttons for the current input

--- button_selection.py
+++ button_selection.py
@@ -10,13 +10,12 @@ def main():
         # Display the current user input
         st.write(f"Current input: {user_input}")
 
-        # Display three buttons for each user input
-        for i in range(len(user_input.split())):
-            for j in range(3):
-                button_choice = st.button(f"{user_input.split()[i]} - Option {j+1}")
-                if button_choice:
-                    user_input = f"{user_input} - Option {j+1}"
-                    break
+        # Display a set of three buttons for the current input
+        for i in range(3):
+            button_choice = st.button(f"{user_input} - Option {i+1}")
+            if button_choice:
+                user_input = f"{user_input} - Option {i+1}"
+                break
 
         # Ask for a new user input
         user_input = st.text_input("Enter your input:", value=user_input[len(user_input.split()[-1]):].strip())
