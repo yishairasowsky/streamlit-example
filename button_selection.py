1bdcb334c4288f8fa7ac0642dd3a88173ecd7633
yishai <yishairasowsky@gmail.com>
2023-03-22 18:58:40 +0200

    create a new variable to store the user's new input value instead of reusing the same user_input variable.

--- button_selection.py
+++ button_selection.py
@@ -15,14 +15,12 @@ def main():
         for i in range(3):
             button_choice = st.button(f"{button_options[i]}")
             if button_choice:
-                user_input = f"{user_input} - {button_options[i]}"
+                user_input_value = user_input + f" - {button_options[i]}"
                 break
         
         # Ask for a new user input and reset the button options
-        user_input = st.text_input("Enter your input:", value=user_input[len(user_input.split()[-1]):].strip())
+        new_user_input = st.text_input("Enter your input:", value=user_input_value[len(user_input.split()[-1]):].strip())
         button_options = ["Option 1", "Option 2", "Option 3"]
-    
-    st.write("Thanks for using the Button Selection App!")
-
-if __name__ == "__main__":
-    main()
+        
+        # Update the user input variable
+        user
