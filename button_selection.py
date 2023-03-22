0c314d825c8252bd47ac34a13a4b9552d780595d
yishai <yishairasowsky@gmail.com>
2023-03-22 19:10:56 +0200

    add main function

--- button_selection.py
+++ button_selection.py
@@ -16,23 +16,27 @@ def get_next_responses(response):
         responses[numbers[i]] = suggestions[i].strip()
     return responses
 
-# Define initial user input
-user_input = st.text_input("Enter some text here:")
+def main()
+    # Define initial user input
+    user_input = st.text_input("Enter some text here:")
 
-# Loop to continue conversation until user cancels or terminates
-while user_input:
-    # Generate response based on user input
-    response = generate_response(user_input)
-    # Get next set of responses from the user via buttons
-    next_responses = get_next_responses(response)
-    # Create buttons for each response option
-    button_container = st.container()
-    cols = {}
-    with button_container:
-        cols['1'], cols['2'], cols['3'] = st.columns(3)
-        for key, val in next_responses.items():
-            if cols[key].button(next_responses[key]):
-                user_input = val
-                break
-        else:
-            user_input = None
+    # Loop to continue conversation until user cancels or terminates
+    while user_input:
+        # Generate response based on user input
+        response = generate_response(user_input)
+        # Get next set of responses from the user via buttons
+        next_responses = get_next_responses(response)
+        # Create buttons for each response option
+        button_container = st.container()
+        cols = {}
+        with button_container:
+            cols['1'], cols['2'], cols['3'] = st.columns(3)
+            for key, val in next_responses.items():
+                if cols[key].button(next_responses[key]):
+                    user_input = val
+                    break
+            else:
+                user_input = None
+
+if __name__ == "__main__":
+    main()
